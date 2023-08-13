7d5c697b55e3ff7543add2fe4572e32c45fb01ac
MuxammilSidd <memuzammilsiddiqui@gmail.com>
2023-08-13 18:19:30 +0500

    Revert "changes removed"
    This reverts commit e6f9c9fada01dc11c9f62bd4ccbe353fd9f35ddf.

---
+++ 09_NoOfDigits.c
@@ -0,0 +1,24 @@
+//  Write a C program to count number of digits in a number.
+#include<stdio.h>
+int main()
+{
+    int num = 10;
+    int dig = 0;
+    int num1 = num;
+    // 234 / 10 = 23
+    // 23 / 10 = 2
+    // 2 / 10 = 0
+
+    while(num!=0)
+    {
+        num = num/10;
+        dig++;
+    }
+
+    printf("\n\tNumber: %d has %d Digits. ",num1, dig);
+    printf("Change");
+    printf("Change22");
+    printf("Change333");
+
+    return 0;
+}
